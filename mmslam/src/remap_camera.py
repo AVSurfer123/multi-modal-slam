bd1c37808e599fd4eecb2186e195093a5e952772
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2022-12-06 02:16:27 -0800

    Adding rotate to remap_camera

--- mmslam/src/remap_camera.py
+++ mmslam/src/remap_camera.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
+import cv2
+from cv_bridge import CvBridge
 from sensor_msgs.msg import CompressedImage, CameraInfo
 
 class Remap:
@@ -8,11 +10,16 @@ class Remap:
     def __init__(self):
         self.image_pub = rospy.Publisher('/camera/image/compressed', CompressedImage, queue_size=5)
         self.info_pub = rospy.Publisher('/camera/camera_info', CameraInfo, queue_size=5)
+        self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, self.image_cb)
         self.info_sub = rospy.Subscriber('/raspicam_node/camera_info', CameraInfo, self.info_cb)
+
         
     def image_cb(self, msg):
-        self.image_pub.publish(msg)
+        img = self.bridge.compressed_imgmsg_to_cv2(msg)
+        rot = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+        img_msg = self.bridge.cv2_to_compressed_imgmsg(rot)
+        self.image_pub.publish(img_msg)
 
     def info_cb(self, msg):
         self.info_pub.publish(msg)
